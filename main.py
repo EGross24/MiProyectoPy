f2b41ebc30df53f7cc36329f51623539a82e2571
EGross24 <er23-0430@unphu.edu.do>
2026-01-17 20:52:15 -0400

    Commit incicial: added main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#er23-0430
+#primera prueba
+#escribe un código sencillo donde se pueda imprimir su nombre y apellido
+
+nombre = "Emily"
+apellido = "Rosario"
+
+print("Mi nombre es:", nombre, "" + apellido)
